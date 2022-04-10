f6c022388de09637e1030ed0e762805e2abfdef3
Guillermo <50501108+guicalare@users.noreply.github.com>
2022-04-10 12:36:45 +0200

    actualizacion de codigo a multi proceso

--- Municipios_geohash/municipios_geohash.py
+++ Municipios_geohash/municipios_geohash.py
@@ -1,7 +1,10 @@
 from geopandas import read_file
 from pandas import merge, read_csv, concat
-from polygeohasher import polygeohasher
+from numpy import array_split
+import multiprocessing
+from datetime import datetime
 from os import listdir
+from polygeohasher import polygeohasher
 
 municipios1 = read_file("/home/guillermo/Descargas/shp/recintos_municipales_inspire_peninbal_etrs89/recintos_municipales_inspire_peninbal_etrs89.shp")
 municipios2 = read_file("/home/guillermo/Descargas/shp/recintos_municipales_inspire_canarias_wgs84/recintos_municipales_inspire_canarias_wgs84.shp")
@@ -17,24 +20,36 @@ municipios.reset_index(inplace=True)
 del(municipios["NATCODE"])
 del(municipios["index"])
 
-del(municipios1)
-del(municipios2)
+def shp2geohash(data, mingeohash, maxgeohash, output_dir, id_process):
+    data.reset_index(inplace=True)
+    del(data["index"])
+    for i in range(data.shape[0]):
+        archivos = listdir(output_dir)
+        data_slice = data[i:i+1]
+        try:
+            if data_slice.shape[0] >= 1:
+                id = data_slice["cod_ine"][i]
+                if f"{id}.csv" not in archivos:
+                    print(f"{id_process} {i} {id}")
+                    primary_df = polygeohasher.create_geohash_list(data_slice, maxgeohash, inner=False)
+                    secondary_df = polygeohasher.geohash_optimizer(primary_df, mingeohash, maxgeohash, maxgeohash)
+                    secondary_df.to_csv(f"{output_dir}{id}.csv", sep="#")
+                else:
+                    print(f"EXISTE {i} -- {id}.csv")
+        except Exception as e:
+            print(e)
 
-for i in range(8131, -1, -1):
-    archivos = listdir("/home/guillermo/Descargas/shp/geohash_shp")
-    data_slice = municipios[i-1:i]
-    try:
-        if data_slice.shape[0] >= 1:
-            id = data_slice["cod_ine"][i-1]
-            if f"{id}.csv" not in archivos:
-                print(f"{i} {id}")
-                primary_df = polygeohasher.create_geohash_list(data_slice, 8, inner=False)
-                secondary_df = polygeohasher.geohash_optimizer(primary_df, 3, 8, 8)
-                secondary_df.to_csv(f"/home/guillermo/Descargas/shp/geohash_shp/{id}.csv", sep="#")
-            else:
-                print(f"EXISTE {i} -- {id}.csv")
-    except Exception as e:
-        print(e)
+def shp2geohash_multiprocessing(num_process, data, mingeohash, maxgeohash, output_dir):
+    splits = array_split(data, num_process)
+    threads = []
+    for index in range(num_process):
+        x = multiprocessing.Process(target=shp2geohash, args=(splits[index], mingeohash, maxgeohash, output_dir, index, ))
+        threads.append(x)
+        x.start()
+    start = datetime.now()
+    for index, thread in enumerate(threads):
+        thread.join()
+    end = datetime.now()
+    print("Tiempo de ejecucion: ", end-start)
 
-datos = pd.concat(map(read_csv, glob.glob(path.join('/home/guillermo/Descargas/shp/geohash_shp', "*.csv"))))
-datos.to_csv("/home/guillermo/Descargas/shp/bb_geohash.csv", sep="#", index=False)
+shp2geohash_multiprocessing(10, municipios, 3, 8, "/home/guillermo/Descargas/shp/prueba/")
